87896e0676999604fccfc4164b548151f852b835
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-11-05 00:36:30 +0300

    js has Map & Set офигеть!

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Symbol.iterator
+# Symbol.iterator [![Build status](https://ci.appveyor.com/api/projects/status/stmb03mhvxtqjt45/branch/master?svg=true)](https://ci.appveyor.com/project/Pavel-A-T/symbol-iterator/branch/master)
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,9 @@
+import canIterate from "../index";
+
+test(('function canIterate'), () => {
+  expect(canIterate("Netology")).toBe(true);
+  expect(canIterate(10)).toBe(false);
+  expect(canIterate(null)).toBe(false);
+  expect(canIterate(new Map())).toBe(true);
+  expect(canIterate(new Set())).toBe(true);
+});
